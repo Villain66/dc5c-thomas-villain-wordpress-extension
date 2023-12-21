0d9694034689499a8000b903bc21ec60e29c66a8
Villain66 <t.villainpro@gmail.com>
2023-12-21 21:33:22 +0100

    Ajout possibilité plusieurs créneaux même journée

--- plugin_reservation_creneau.php
+++ plugin_reservation_creneau.php
@@ -10,16 +10,16 @@
 function reservation_form_shortcode() {
     ob_start();
     ?>
-<!-- Formulaire de réservation HTML -->
-<form method="post" action="">
-    <!-- Champs du formulaire -->
-    <input type="text" name="user_first_name" placeholder="Prénom" required>
-    <input type="text" name="user_name" placeholder="Nom" required>
-    <input type="email" name="user_email" placeholder="Adresse e-mail" required>
-    <input type="date" name="reservation_date" required>
-    <input type="submit" name="submit_reservation" value="Réserver">
-</form>
-
+    <!-- Formulaire de réservation HTML -->
+    <form method="post" action="">
+        <!-- Champs du formulaire -->
+        <input type="text" name="user_first_name" placeholder="Prénom" required>
+        <input type="text" name="user_name" placeholder="Nom" required>
+        <input type="email" name="user_email" placeholder="Adresse e-mail" required>
+        <input type="date" name="reservation_date" required>
+        <input type="time" name="reservation_time" required>
+        <input type="submit" name="submit_reservation" value="Réserver">
+    </form>
 
     <!-- Ajouter le script JavaScript pour le pop-up -->
     <script>
@@ -45,11 +45,12 @@ function process_reservation() {
         $user_name = sanitize_text_field($_POST['user_name']);
         $user_email = sanitize_email($_POST['user_email']);
         $reservation_date = sanitize_text_field($_POST['reservation_date']);
+        $reservation_time = sanitize_text_field($_POST['reservation_time']);
 
         // Envoie un e-mail aux administrateurs
         $to = get_option('admin_email');
         $subject = 'Nouvelle réservation de créneau';
-        $message = "Prénom: $user_first_name\nNom: $user_name\nE-mail: $user_email\nDate de réservation: $reservation_date";
+        $message = "Prénom: $user_first_name\nNom: $user_name\nE-mail: $user_email\nDate de réservation: $reservation_date\nHeure de réservation: $reservation_time";
 
         wp_mail($to, $subject, $message);
 
